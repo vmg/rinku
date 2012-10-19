c684ac1d88ecd19d1b884a254a1eb186de59248b
Shajith Chacko <shajith@zendesk.com>
2012-10-19 14:54:58 -0700

    Require UTF-8 compatible input, use incoming encoding in the string yielded to block.

--- test/autolink_test.rb
+++ test/autolink_test.rb
@@ -32,7 +32,7 @@ class RedcarpetAutolinkTest < Test::Unit::TestCase
     Rinku.skip_tags = nil
     assert_not_equal Rinku.auto_link(url), url
   end
-  
+
   def test_auto_link_with_single_trailing_punctuation_and_space
     url = "http://www.youtube.com"
     url_result = generate_result(url)
@@ -138,7 +138,7 @@ This is just a test. <a href="http://www.pokemon.com">http://www.pokemon.com</a>
     url2 = "http://www.ruby-doc.org/core/Bar.html"
 
     assert_equal %(<p><a href="#{url1}">#{url1}</a><br /><a href="#{url2}">#{url2}</a><br /></p>), Rinku.auto_link("<p>#{url1}<br />#{url2}<br /></p>")
-  end  
+  end
 
   def test_block
     link = Rinku.auto_link("Find ur favorite pokeman @ http://www.pokemon.com") do |url|
@@ -147,24 +147,6 @@ This is just a test. <a href="http://www.pokemon.com">http://www.pokemon.com</a>
     end
 
     assert_equal link, "Find ur favorite pokeman @ <a href=\"http://www.pokemon.com\">POKEMAN WEBSITE</a>"
-  end
-
-  def test_block_encoding
-    url = "http://example.com/х"
-    assert_equal "UTF-8", url.encoding.to_s
-
-    link = Rinku.auto_link(url) do |url|
-      assert_equal "UTF-8", url.encoding.to_s
-      url
-    end
-
-    assert_equal link.encoding.to_s, "UTF-8"
-  end
-
-  def test_links_with_cyrillic_x
-    url = "http://example.com/х"
-
-    assert_linked "<a href=\"#{url}\">#{url}</a>", url
   end
 
   def test_autolink_works
@@ -302,6 +284,36 @@ This is just a test. <a href="http://www.pokemon.com">http://www.pokemon.com</a>
 
       ret = Rinku.auto_link str
       assert_equal str.encoding, ret.encoding
+    end
+
+    def test_block_encoding
+      url = "http://example.com/х"
+      assert_equal "UTF-8", url.encoding.to_s
+
+      link = Rinku.auto_link(url) do |u|
+        assert_equal "UTF-8", u.encoding.to_s
+        u
+      end
+
+      assert_equal link.encoding.to_s, "UTF-8"
+
+      url = "http://www.bash.org"
+      url.encode! 'binary'
+
+      link = Rinku.auto_link(url) do |u|
+        assert_equal url.encoding.to_s, u.encoding.to_s
+        u
+      end
+
+      assert_equal url.encoding, link.encoding
+
+    end
+
+    def test_bad_encoding
+      url = "http://example.com/ümlaut".encode("ISO-8859-1")
+      assert_raise ArgumentError do
+        Rinku.auto_link(url)
+      end
     end
   end
 
