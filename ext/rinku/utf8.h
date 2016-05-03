e21cfedb41e3c9742f9c416f0ca8196decdece93
Kerri Miller <kerrizor@github.com>
2016-05-03 11:37:56 -0700

    Add missing function declaration

--- ext/rinku/utf8.h
+++ ext/rinku/utf8.h
@@ -8,6 +8,7 @@ typedef int bufsize_t;
 int rinku_ispunct(char c);
 int rinku_isspace(char c);
 
+static int utf8proc_charlen(const uint8_t *str, bufsize_t str_len);
 int utf8proc_iterate(const uint8_t *str, bufsize_t str_len, int32_t *dst);
 int utf8proc_is_space(int32_t uc);
 
