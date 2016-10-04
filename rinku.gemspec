a9a22f62c13a7a854b8145158bd6d8c317c81b57
Michael Grosser <michael@grosser.it>
2016-10-04 13:09:14 -0700

    make tests run ... they fail with minitest 4

--- rinku.gemspec
+++ rinku.gemspec
@@ -39,5 +39,5 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency "rake"
   s.add_development_dependency "rake-compiler"
-  s.add_development_dependency "minitest"
+  s.add_development_dependency "minitest", ">= 5.0"
 end
--- test/autolink_test.rb
+++ test/autolink_test.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
-rootdir = File.dirname(File.dirname(__FILE__))
-$LOAD_PATH.unshift "#{rootdir}/lib"
+require 'bundler/setup'
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'minitest/autorun'
 require 'cgi'
